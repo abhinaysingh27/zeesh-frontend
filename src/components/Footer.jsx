a6eb977d2576920876ffeed7d88ac4b3e20b3c97
abhinaysingh27 <abhinay.chiku1234@gmail.com>
2025-09-02 00:56:11 +0530

    frontend done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>Boring road, patna</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Abhinay Singh</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
